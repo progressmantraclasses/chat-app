c6cad2c6f0bb18a6bed6cdc5f1d9bc1d74597385
Shivam <rajmishra112001@gmail.com>
2024-10-21 03:54:29 +0530

    fronted url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",  // Frontend URL
+        origin: "https://unknown-chats.web.app/",  // Frontend URL
         methods: ["GET", "POST"]
     }
 });
